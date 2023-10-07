ff8a44b9e4a019d0c9820fdf0b93904b82fb6b7d
Christian Deacon <christianmdeacon@gmail.com>
2023-10-07 02:47:37 +0000

    Start making progress in main program.

--- main.go
+++ main.go
@@ -1,7 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bestserversio/spy/internal/config"
+)
+
+const VERSION = "1.0.0"
+const HELPMENU = `
+./spy [OPTIONS]\n\n
+-l --list => Prints config and exits.\n
+-v --version => Prints version and exits.\n
+-h --help => Prints help menu and exits.\n
+-c --cfg => Path to config file.\n
+`
 
 func main() {
-	fmt.Println("Test.")
+	// Command line options and parse command line.
+	var list bool
+	var version bool
+	var help bool
+
+	flag.BoolVar(&list, "l", false, "Prints config settings and exits.")
+	flag.BoolVar(&list, "list", false, "Prints config settings and exits.")
+
+	flag.BoolVar(&version, "v", false, "Prints version and exits.")
+	flag.BoolVar(&version, "version", false, "Prints number and exits.")
+
+	flag.BoolVar(&help, "h", false, "Prints help menu and exits.")
+	flag.BoolVar(&help, "help", false, "Prints help menu and exits.")
+
+	cfgPath := flag.String("cfg", "/etc/bestservers/spy.json", "Path to config file.")
+
+	flag.Parse()
+
+	// Check for version.
+	if version {
+		fmt.Println(VERSION)
+
+		os.Exit(0)
+	}
+
+	// Check for help menu.
+	if version {
+		fmt.Print(HELPMENU)
+
+		os.Exit(0)
+	}
+
+	// Initialize config.
+	cfg := config.Config{}
+
+	// Load defaults.
+	cfg.LoadDefaults()
+
+	// Attempt to load config.
+	err := cfg.LoadFromFs(*cfgPath)
+
+	if err != nil {
+		fmt.Println("Error loading config file. Resorting to defaults...")
+		fmt.Println(err)
+	}
+
+	// Check if we want to print our config settings.
+	if list {
+		cfg.PrintConfig()
+
+		os.Exit(0)
+	}
+
+	// Make a signal.
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sigc
+
+	fmt.Println("Exiting...")
+	os.Exit(0)
+
 }
