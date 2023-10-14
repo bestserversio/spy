6647510bb80a4edef9b267e0e4b0c2581e76f0fd
Christian Deacon <christianmdeacon@gmail.com>
2023-10-14 08:04:09 +0000

    Add verbose and timeout fields to config structure.

--- internal/config/types.go
+++ internal/config/types.go
@@ -1,8 +1,10 @@
 package config
 
 type Config struct {
+	Verbose       int    `json:"verbose"`
 	Host          string `json:"host"`
 	EndPoint      string `json:"endpoint"`
 	Authorization string `json:"authorization"`
 	Ssl           bool   `json:"ssl"`
+	Timeout       uint   `json:"timeout"`
 }
