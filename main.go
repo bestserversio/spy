c7959a02dee7632d789e8909e7cb8e284641be6a
Christian Deacon <christianmdeacon@gmail.com>
2024-05-17 21:43:50 -0400

    Start working on VMS timer and functionality.

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
+	"time"
 
 	"github.com/bestserversio/spy/internal/config"
+	"github.com/bestserversio/spy/internal/servers"
+	"github.com/bestserversio/spy/internal/utils"
+	"github.com/bestserversio/spy/internal/vms"
 )
 
 const VERSION = "1.0.0"
@@ -18,6 +25,89 @@ const HELPMENU = `
 -h --help => Prints help menu and exits.\n
 -c --cfg => Path to config file.\n
 `
+
+func DoVms(cfg *config.Config) {
+	if !cfg.Vms.Enabled {
+		return
+	}
+
+	// Retrieve random app ID.
+	rand.Seed(time.Now().UnixNano())
+
+	randIndex := rand.Intn(len(cfg.Vms.AppIds))
+
+	appId := cfg.Vms.AppIds[randIndex]
+
+	utils.DebugMsg(4, cfg.Verbose, "[VMS] Using (random) app ID %d.", appId)
+
+	// Retrieve servers.
+	allServers, err := vms.RetrieveServers(cfg, appId)
+
+	if err != nil {
+		utils.DebugMsg(1, cfg.Verbose, "[VMS] Failed to retrieve servers for app %d due to error.", appId)
+		utils.DebugMsg(1, cfg.Verbose, err.Error())
+
+		return
+	}
+
+	utils.DebugMsg(4, cfg.Verbose, "[VMS] Retrieved %d servers from app ID %d.", len(allServers), appId)
+
+	var serversToUpdate []servers.Server
+
+	// Loop through all servers from VMS result.
+	for _, srv := range allServers {
+		// Create new servers object from servers package and assign basic values.
+		newSrv := servers.Server{
+			HostName: new(string),
+			CurUsers: new(int),
+			MaxUsers: new(int),
+			MapName:  new(string),
+			Ip:       new(string),
+			Port:     new(int),
+		}
+		*newSrv.HostName = srv.HostName
+		*newSrv.CurUsers = srv.Players
+		*newSrv.MaxUsers = srv.MaxPlayers
+		*newSrv.MapName = srv.Map
+
+		// We need to split IP and port from address.
+		split := strings.Split(srv.Address, ":")
+
+		if len(split) > 1 {
+			*newSrv.Ip = split[0]
+
+			portNum, err := strconv.Atoi(split[1])
+
+			if err != nil {
+				utils.DebugMsg(1, cfg.Verbose, "")
+
+				return
+			}
+
+			*newSrv.Port = portNum
+		}
+
+		serversToUpdate = append(serversToUpdate, newSrv)
+	}
+
+	if len(serversToUpdate) < 1 {
+		utils.DebugMsg(3, cfg.Verbose, "[VMS] Found no servers for app ID %d.", appId)
+
+		return
+	}
+
+	// Add/update servers.
+	cnt, err := servers.AddServers(cfg, serversToUpdate, true)
+
+	if err != nil {
+		utils.DebugMsg(1, cfg.Verbose, "[VMS] Failed to add/update servers for app ID %d due to error.", appId)
+		utils.DebugMsg(1, cfg.Verbose, err.Error())
+
+		return
+	}
+
+	utils.DebugMsg(2, cfg.Verbose, "[VMS] Added/Updated %d servers!", cnt)
+}
 
 func main() {
 	// Command line options and parse command line.
@@ -66,6 +156,8 @@ func main() {
 		fmt.Println(err)
 	}
 
+	utils.DebugMsg(4, cfg.Verbose, "[MAIN] Config loaded...")
+
 	// Check if we want to print our config settings.
 	if list {
 		cfg.PrintConfig()
@@ -73,13 +165,30 @@ func main() {
 		os.Exit(0)
 	}
 
+	// Create VMS timer.
+	utils.DebugMsg(4, cfg.Verbose, "[MAIN] Creating VMS timer...")
+	vmsTicker := time.NewTicker(time.Second * time.Duration(cfg.Vms.Interval))
+	vmsQuit := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case <-vmsTicker.C:
+				DoVms(&cfg)
+			case <-vmsQuit:
+				vmsTicker.Stop()
+
+				return
+			}
+		}
+	}()
+
 	// Make a signal.
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
 
 	<-sigc
 
-	fmt.Println("Exiting...")
+	utils.DebugMsg(0, cfg.Verbose, "[MAIN] Exiting...")
 	os.Exit(0)
-
 }
