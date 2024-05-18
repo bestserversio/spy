189c8134dd14a36cb61fc6e9c44c0fd5ba926649
Christian Deacon <christianmdeacon@gmail.com>
2024-05-17 21:43:02 -0400

    Add more default config values.

--- internal/config/defaults.go
+++ internal/config/defaults.go
@@ -1,11 +1,15 @@
 package config
 
 func (cfg *Config) LoadDefaults() {
-	cfg.Api.Host = "localhost"
+	cfg.Api.Host = "http://localhost"
 	cfg.Api.Authorization = ""
 	cfg.Api.Ssl = false
 	cfg.Api.Timeout = 5
 
 	cfg.Vms.Enabled = false
+	cfg.Vms.Interval = 120
 	cfg.Vms.Timeout = 5
+
+	cfg.Scanner.MinWait = 10
+	cfg.Scanner.MaxWait = 15
 }
