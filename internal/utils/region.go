bbea0b2adb251cd0ff9bb8cca324615eb24c0918
Christian Deacon <christianmdeacon@gmail.com>
2024-06-03 20:00:32 -0400

    We don't need break with switch statements in Go.

--- internal/utils/region.go
+++ internal/utils/region.go
@@ -6,42 +6,20 @@ func GetRegion(region int) string {
 	switch region {
 	case 0:
 		ret = "US"
-
-		break
 	case 1:
 		ret = "US"
-
-		break
-
 	case 2:
 		ret = "SA"
-
-		break
-
 	case 3:
 		ret = "EU"
-
-		break
-
 	case 4:
 		ret = "AS"
-
-		break
-
 	case 5:
 		ret = "AU"
-
-		break
-
 	case 6:
 		ret = "ME"
-
-		break
-
 	case 7:
 		ret = "AF"
-
-		break
 	}
 
 	return ret
